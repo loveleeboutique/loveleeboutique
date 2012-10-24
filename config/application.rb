5ad3f9eb47c5a015d8dc10c14f23db3104a75471
Oren Weichselbaum <oren@Orens-MacBook-Air.local>
2012-10-24 19:44:38 -0400

    asset pipeline for heroku

--- config/application.rb
+++ config/application.rb
@@ -17,7 +17,7 @@ end
 
 module Loveleeboutique
   class Application < Rails::Application
-    
+    config.assets.initialize_on_precompile = false
     config.to_prepare do
       # Load application's model / class decorators
       Dir.glob(File.join(File.dirname(__FILE__), "../app/**/*_decorator*.rb")) do |c|
