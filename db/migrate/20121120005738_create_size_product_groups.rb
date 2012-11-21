6a601ee70a87968a806cfadca5e7e0c25b31d1f2
Oren Weichselbaum <oren@Orens-MacBook-Air.local>
2012-11-21 14:25:29 -0500

    heroku migrate test

--- db/migrate/20121120005738_create_size_product_groups.rb
+++ db/migrate/20121120005738_create_size_product_groups.rb
@@ -3,6 +3,8 @@ class CreateSizeProductGroups < ActiveRecord::Migration
     Spree::OptionType.all.each do |ot|
       ot.option_values.each do |ov|
         pg = Spree::ProductGroup.create(:name => ov.name)
+        puts pg.inspect
+        puts pg.errors.empty?
         ps = pg.product_scopes.create(:name => "with_option_value", :arguments => [ot.name, ov.name])
         pg = Spree::ProductGroup.find(pg.id)
         pg.update_memberships
