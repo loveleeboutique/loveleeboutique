dc8cf8ed00300ca73f509db8f2a8c5bff6d7528d
Oren Weichselbaum <oren@Orens-MacBook-Air.local>
2012-11-26 19:34:33 -0500

    fix image sizing

--- app/models/spree/app_configuration_decorator.rb
+++ app/models/spree/app_configuration_decorator.rb
@@ -1,5 +1,6 @@
 module Spree
   AppConfiguration.class_eval do
     preference :products_per_page, :integer, :default => 20
+    preference :attachment_styles, :string, :default => "{\"mini\":\"152x152>\",\"small\":\"227x269>\",\"product\":\"910x1374>\",\"large\":\"600x600>\"}"
   end
 end
