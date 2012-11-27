ac9d7c30e85882c48bc5d41643ed71846517b294
Eric Galperin <edg205@gmail.com>
2012-11-26 20:39:53 -0500

    migration hell... hell i say!!!

--- db/migrate/20121120020505_add_colors.rb
+++ db/migrate/20121120020505_add_colors.rb
@@ -6,7 +6,7 @@ class AddColors < ActiveRecord::Migration
     taxon = taxonomy.taxons.root
     taxon = Spree::Taxon.create(:name => "Blue", :parent_id => taxon.id)
     mpg = Spree::ProductGroup.create(:name => taxon.name.downcase)
-    ["denim", "indigo", "light blue", "navy blue", "royal blue", "teal", "turquoise"].each do |sub|
+    ["denim", "blue","indigo", "light blue", "navy blue", "royal blue", "teal", "turquoise"].each do |sub|
       Spree::Taxon.create(:name => sub.titleize, :parent_id => taxon.id)
       ov = color.option_values.find_or_create_by_name(:presentation => sub.titleize, :name => sub.to_url)
       pg = Spree::ProductGroup.create(:name => ov.name)
@@ -21,7 +21,7 @@ class AddColors < ActiveRecord::Migration
     taxon = taxonomy.taxons.root
     taxon = Spree::Taxon.create(:name => "Black", :parent_id => taxon.id)
     mpg = Spree::ProductGroup.create(:name => taxon.name.downcase)
-    ["grey", "charcoal"].each do |sub|
+    ["grey", "black", "charcoal"].each do |sub|
       Spree::Taxon.create(:name => sub.titleize, :parent_id => taxon.id)
       ov = color.option_values.create(:presentation => sub.titleize, :name => sub.to_url)
       pg = Spree::ProductGroup.create(:name => ov.name)
@@ -36,7 +36,7 @@ class AddColors < ActiveRecord::Migration
     taxon = taxonomy.taxons.root
     taxon = Spree::Taxon.create(:name => "Brown", :parent_id => taxon.id)
     mpg = Spree::ProductGroup.create(:name => taxon.name.downcase)
-    ["beige", "chocolate", "copper", "rust", "sand", "tan"].each do |sub|
+    ["beige", "brown","chocolate", "copper", "rust", "sand", "tan"].each do |sub|
       Spree::Taxon.create(:name => sub.titleize, :parent_id => taxon.id)
       ov = color.option_values.create(:presentation => sub.titleize, :name => sub.to_url)
       pg = Spree::ProductGroup.create(:name => ov.name)
@@ -51,7 +51,7 @@ class AddColors < ActiveRecord::Migration
     taxon = taxonomy.taxons.root
     taxon = Spree::Taxon.create(:name => "Green", :parent_id => taxon.id)
     mpg = Spree::ProductGroup.create(:name => taxon.name.downcase)
-    ["army green", "bright green", "forest green", "olive", "teal", "turquoise"].each do |sub|
+    ["army green", "green", "bright green", "forest green", "olive", "teal", "turquoise"].each do |sub|
       Spree::Taxon.create(:name => sub.titleize, :parent_id => taxon.id)
       ov = color.option_values.create(:presentation => sub.titleize, :name => sub.to_url)
       pg = Spree::ProductGroup.create(:name => ov.name)
@@ -66,7 +66,7 @@ class AddColors < ActiveRecord::Migration
     taxon = taxonomy.taxons.root
     taxon = Spree::Taxon.create(:name => "Orange", :parent_id => taxon.id)
     mpg = Spree::ProductGroup.create(:name => taxon.name.downcase)
-    ["peach", "rust", "salmon"].each do |sub|
+    ["peach", "rust", "orange", "salmon"].each do |sub|
       Spree::Taxon.create(:name => sub.titleize, :parent_id => taxon.id)
       ov = color.option_values.create(:presentation => sub.titleize, :name => sub.to_url)
       pg = Spree::ProductGroup.create(:name => ov.name)
@@ -81,7 +81,7 @@ class AddColors < ActiveRecord::Migration
     taxon = taxonomy.taxons.root
     taxon = Spree::Taxon.create(:name => "Pink", :parent_id => taxon.id)
     mpg = Spree::ProductGroup.create(:name => taxon.name.downcase)
-    ["bright pink", "fuchsia", "hot pink", "lavender", "magenta", "rose"].each do |sub|
+    ["bright pink", "fuchsia", "pink","hot pink", "lavender", "magenta", "rose"].each do |sub|
       Spree::Taxon.create(:name => sub.titleize, :parent_id => taxon.id)
       ov = color.option_values.create(:presentation => sub.titleize, :name => sub.to_url)
       pg = Spree::ProductGroup.create(:name => ov.name)
@@ -96,7 +96,7 @@ class AddColors < ActiveRecord::Migration
     taxon = taxonomy.taxons.root
     taxon = Spree::Taxon.create(:name => "Purple", :parent_id => taxon.id)
     mpg = Spree::ProductGroup.create(:name => taxon.name.downcase)
-    ["fuchsia", "lavender"].each do |sub|
+    ["fuchsia", "purple", "lavender"].each do |sub|
       Spree::Taxon.create(:name => sub.titleize, :parent_id => taxon.id)
       ov = color.option_values.create(:presentation => sub.titleize, :name => sub.to_url)
       pg = Spree::ProductGroup.create(:name => ov.name)
@@ -111,7 +111,7 @@ class AddColors < ActiveRecord::Migration
     taxon = taxonomy.taxons.root
     taxon = Spree::Taxon.create(:name => "Red", :parent_id => taxon.id)
     mpg = Spree::ProductGroup.create(:name => taxon.name.downcase)
-    ["brick", "burgundy", "crimson", "dark red", "magenta", "rose"].each do |sub|
+    ["brick", "burgundy", "red", "crimson", "dark red", "magenta", "rose"].each do |sub|
       Spree::Taxon.create(:name => sub.titleize, :parent_id => taxon.id)
       ov = color.option_values.create(:presentation => sub.titleize, :name => sub.to_url)
       pg = Spree::ProductGroup.create(:name => ov.name)
@@ -126,7 +126,7 @@ class AddColors < ActiveRecord::Migration
     taxon = taxonomy.taxons.root
     taxon = Spree::Taxon.create(:name => "White", :parent_id => taxon.id)
     mpg = Spree::ProductGroup.create(:name => taxon.name.downcase)
-    ["beige", "cream", "tan"].each do |sub|
+    ["beige", "cream", "white", "tan"].each do |sub|
       Spree::Taxon.create(:name => sub.titleize, :parent_id => taxon.id)
       ov = color.option_values.create(:presentation => sub.titleize, :name => sub.to_url)
       pg = Spree::ProductGroup.create(:name => ov.name)
@@ -141,7 +141,7 @@ class AddColors < ActiveRecord::Migration
     taxon = taxonomy.taxons.root
     taxon = Spree::Taxon.create(:name => "Yellow", :parent_id => taxon.id)
     mpg = Spree::ProductGroup.create(:name => taxon.name.downcase)
-    ["cream", "gold", "saffron", "tan"].each do |sub|
+    ["cream", "gold", "saffron", "yellow", "tan"].each do |sub|
       Spree::Taxon.create(:name => sub.titleize, :parent_id => taxon.id)
       ov = color.option_values.create(:presentation => sub.titleize, :name => sub.to_url)
       pg = Spree::ProductGroup.create(:name => ov.name)
