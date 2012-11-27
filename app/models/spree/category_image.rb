ca4bdb340f274de1e5128cc894e58de4dca33dbc
Oren Weichselbaum <oren@Orens-MacBook-Air.local>
2012-11-26 20:18:49 -0500

    fix images for s3

--- app/models/spree/category_image.rb
+++ app/models/spree/category_image.rb
@@ -7,21 +7,22 @@ module Spree
 
     has_attached_file :attachment,
                       :styles => {
-                                    :banner => "942x308",
-                                    :thumb => "100x100" },
+                          :banner => "942x308",
+                          :thumb => "100x100"},
                       default_style: :large,
-                      url: %{/spree/categories/:id/:style/:basename.:extension},
+                      url: %{categories/:id/:style/:basename.:extension},
                       path: %{categories/:id/:style/:basename.:extension}
 
+    include Spree::Core::S3Support
+    supports_s3 :attachment
 
-
-    #if Spree::Config[:use_s3]
-    #    s3_creds = {:access_key_id => Spree::Config[:s3_access_key], :secret_access_key => Spree::Config[:s3_secret], :bucket => Spree::Config[:s3_bucket]}
-    #    Spree::Image.attachment_definitions[:image][:storage] = :s3
-    #    Spree::Image.attachment_definitions[:image][:s3_credentials] = s3_creds
-    #    Spree::Image.attachment_definitions[:image][:s3_headers] = ActiveSupport::JSON.decode(Spree::Config[:s3_headers])
-    #    Spree::Image.attachment_definitions[:image][:bucket] = Spree::Config[:s3_bucket]
-    #  end
+    if Spree::Config[:use_s3]
+      s3_creds = {:access_key_id => Spree::Config[:s3_access_key], :secret_access_key => Spree::Config[:s3_secret], :bucket => Spree::Config[:s3_bucket]}
+      Spree::Image.attachment_definitions[:attachment][:storage] = :s3
+      Spree::Image.attachment_definitions[:attachment][:s3_credentials] = s3_creds
+      Spree::Image.attachment_definitions[:attachment][:s3_headers] = ActiveSupport::JSON.decode(Spree::Config[:s3_headers])
+      Spree::Image.attachment_definitions[:attachment][:bucket] = Spree::Config[:s3_bucket]
+    end
 
     # if there are errors from the plugin, then add a more meaningful message
     def no_attachment_errors
