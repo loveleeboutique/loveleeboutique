5d3cd34e97598141ef0c7aa9d48ab3038c67daaf
Oren Weichselbaum <oren@Orens-MacBook-Air.local>
2012-11-28 13:14:00 -0500

    change ordering of variants on product page

--- app/models/spree/variant_decorator.rb
+++ app/models/spree/variant_decorator.rb
@@ -1,13 +1,13 @@
 module Spree
   Variant.class_eval do
     def options_text
-          values = self.option_values.sort_by(&:position)
+          values = self.option_values.sort_by(&:option_type_id)
 
           values.map! do |ov|
             "#{ov.presentation}"
           end
 
-          values.to_sentence({ :words_connector => ", ", :two_words_connector => ", " })
+          values.to_sentence({ :words_connector => " ", :two_words_connector => " " }).gsub(" ", '<br/>').html_safe
     end
   end
 end
